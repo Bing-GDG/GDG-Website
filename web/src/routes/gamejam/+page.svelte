189485a9dbc54f7fe9803d259b73aaeda7a49b6f
Tammyhi <85895125+Tammyhi@users.noreply.github.com>
2026-03-08 15:57:16 -0400

    test: Placeholder text, teaching Tammy svelte and tailwind

---
+++ web/src/routes/gamejam/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let someVar: number = $state(4);
+
+    function add(a: number, b: number): number {
+        return a + b;
+    }
+</script>
+
+<div class="flex flex-col w-screen h-screen items-center justify-center">
+    <h1 class="font-bold text-2xl">{add(someVar, 2)}</h1>
+    <button class="border border-black" onclick={() => {
+        console.log("awfawflajk");
+        someVar++;
+    }}>ADD</button>
+</div>
